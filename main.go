a93d9ad4105b7ddc2a19b1b2fd544151918ad773
liangyouheng <liangyouheng@ghzhushou.com>
2020-08-21 19:25:42 +0800

    update

--- main.go
+++ main.go
@@ -3,21 +3,19 @@ package main
 import (
 	"fmt"
 	"gomq/mq"
+	"gomq/redis"
 )
 
 func main()  {
-	job := mq.NewJob("test",1)
+	job := mq.NewJob("test", 1, redis.GetPool(), func(message mq.Message) bool {
+		fmt.Println(message)
+		return true
+	})
 	data := map[string]interface{}{
 		"name" : "hello",
 		"age" : 18,
 	}
-	job.Push(data)
-
-	_, _ = job.Handle(func(message mq.Message) bool {
-		data := message.Data
-		fmt.Println(data)
-		return true
-	})
+	_ = job.Push(data)
 
 	for {
 		select {
--- mq/job.go
+++ mq/job.go
@@ -1,3 +1,4 @@
+//golang+redis 简单版mq ,有ack功能
 package mq
 
 import (
@@ -6,27 +7,161 @@ import (
 	"errors"
 	"fmt"
 	"github.com/mediocregopher/radix/v3"
-	"gomq/redis"
 	"gopkg.in/mgo.v2/bson"
 	"math/big"
+	"strconv"
+	"sync"
+	"time"
 )
 
-type Job struct {
-	Name string
-	Num           int64
-	List   []*Queue
+type job struct {
+	name string
+	num           int64
+	doingTable  string
+	list   []*queue
+	redisConn  *radix.Pool
+	handler  func(message Message) bool
 }
 
-func NewJob(name string, Num int64) *Job {
-	job := new(Job)
-	job.Name = fmt.Sprintf("mq:job:name:%v", name)
-	job.Num = Num
-	job.initQueueList()
-	return job
+//工作池
+var jobPool struct{
+	job map[string]*job
+	lock         sync.RWMutex
 }
 
-type Queue struct {
-	Name   string
+func NewJob(name string, Num int64, conn *radix.Pool, handler func(message Message) bool) *job {
+	name = fmt.Sprintf("mq:job:name:%v", name)
+	jobPool.lock.RLock()
+	if job,ok := jobPool.job[name]; ok {
+		jobPool.lock.RUnlock()
+		//如果已经存在job 但是处理函数是空,但是传过来的处理函数非空,就重新装载处理函数
+		if job.handler == nil && handler != nil {
+			jobPool.lock.Lock()
+			jobPool.job[name].handler = handler
+			go jobPool.job[name].handle()
+			jobPool.lock.Unlock()
+		}
+		return job
+	}
+	jobPool.lock.RUnlock()
+
+	if conn == nil {
+		panic(errors.New("bad radix conn"))
+	}
+
+	jobPool.lock.Lock()
+	newJob := new(job)
+	newJob.name = name
+	newJob.num = Num
+	newJob.doingTable = newJob.name + ":doing"
+	newJob.redisConn = conn
+	newJob.handler = handler
+	newJob.initQueueList()
+
+	go newJob.rollbackAck()
+	go newJob.handle()
+
+	if jobPool.job == nil {
+		jobPool.job = make(map[string]*job)
+	}
+	jobPool.job[name] = newJob
+	jobPool.lock.Unlock()
+	return newJob
+}
+
+
+
+func (job *job) initQueueList() {
+	job.list = make([]*queue, job.num)
+	var i int64
+	for i = 0; i < job.num; i++ {
+		job.list[i] = newQueue(job.name, i, job.redisConn)
+	}
+}
+
+//负责ack的函数，采用有序集合，分数使用时间戳，获取时间戳是0到目前的时间范围的message,获取到的消息就是需要返回给队列的数据
+func (job *job) rollbackAck () {
+	con := job.redisConn
+	for {
+		var value []string
+		var message Message
+		expireTime := strconv.FormatInt(time.Now().Unix(), 10)
+		err := con.Do(radix.FlatCmd(&value, "ZRANGEBYSCORE", job.doingTable, "0", expireTime))
+		if err != nil || len(value) == 0 {
+			time.Sleep(time.Second * 1)
+			continue
+		}
+		for _, v := range value {
+			_ = json.Unmarshal([]byte(v), &message)
+			err = job.getList().push(message)
+			if err == nil {
+				err = con.Do(radix.Cmd(&value, "ZREM", job.doingTable, v))
+			}
+			fmt.Println("ack:",v)
+		}
+	}
+}
+
+func (job *job) getList() *queue {
+	//随机分配到某个list
+	key := rangeRand(0, job.num - 1)
+	return job.list[key]
+}
+
+func (job *job) Push(data interface{}) (err error) {
+	queue := job.getList()
+	message := Message{
+		Id: bson.NewObjectId().Hex() ,
+		Data: data,
+	}
+	err = queue.push(message)
+	return
+}
+
+func (job *job) BatchPush(data []interface{}) (err error) {
+	queue := job.getList()
+	var messages []Message
+	for _, v := range data {
+		message := Message{
+			Id: bson.NewObjectId().Hex() ,
+			Data: v,
+		}
+		messages = append(messages, message)
+	}
+
+	err = queue.batchPush(messages)
+	return
+}
+
+func (job *job) handle() {
+	if job.handler == nil {
+		return
+	}
+	var i int64
+	for i = 0 ; i < job.num; i++ {
+		queue := job.list[i]
+		go func() {
+			for {
+				message, err := queue.receiveMessage()
+				if err != nil {
+					continue
+				}
+				if job.handler(message) {
+					err = queue.deleteMessage(message)
+					if err != nil {
+						fmt.Println(err)
+					}
+				}
+			}
+		}()
+	}
+}
+
+
+type queue struct {
+	name   string
+	doingTable  string
+	redisConn *radix.Pool
 }
 
 type Message struct {
@@ -34,13 +169,13 @@ type Message struct {
 	Data interface{}
 }
 
-func NewQueue(jobName string, i int64) *Queue {
+func newQueue(jobName string, i int64, redisConn *radix.Pool) *queue {
 	key := fmt.Sprintf(":list-%d", i)
-	queue := &Queue{Name: jobName+key}
+	queue := &queue{name: jobName+key, doingTable:jobName + ":doing", redisConn : redisConn}
 	return queue
 }
 
-func (queue *Queue) push (message Message) (err error)  {
+func (queue *queue) push (message Message) (err error)  {
 	dataByte ,err := json.Marshal(message)
 
 	if err != nil {
@@ -51,80 +186,90 @@ func (queue *Queue) push (message Message) (err error)  {
 		err = errors.New("data is nil")
 		return
 	}
-	con := redis.GetPool()
+	con := queue.redisConn
 	var ok bool
-	err = con.Do(radix.Cmd(&ok, "RPUSH", queue.Name, string(dataByte)))
+	err = con.Do(radix.Cmd(&ok, "LPUSH", queue.name, string(dataByte)))
 	return
 }
 
-func (queue *Queue) receive() (message Message , err error)  {
-	con := redis.GetPool()
+func (queue *queue) batchPush (messages []Message) (err error)  {
+	if len(messages) == 0{
+		err = errors.New("data is nil")
+		return
+	}
+	addArgs := []string{queue.name}
+	for _, message := range messages {
+		dataByte ,_ := json.Marshal(message)
+		addArgs = append(addArgs, string(dataByte))
+	}
+
+	con := queue.redisConn
+	var ok bool
+	err = con.Do(radix.Cmd(&ok, "LPUSH", addArgs...))
+	return
+}
+
+func (queue *queue) receiveMessage() (message Message , err error)  {
+	con := queue.redisConn
 	var val []string
-	err = con.Do(radix.Cmd(&val, "BLPOP", queue.Name, "10"))
+	err = con.Do(radix.Cmd(&val, "BRPOP", queue.name, "10"))
 	if err != nil {
 		return
 	}
 	if len(val) == 2 {
 		value := []byte(val[1])
 		json.Unmarshal(value, &message)
+		err = queue.addAck(message)
+		if err != nil {
+			for k :=0 ; k < 4; k ++ {
+				err = queue.push(message)
+				if err == nil {
+					break
+				}
+			}
+		}
 	} else {
 		err = errors.New("no message")
 	}
 	return
 }
 
-func (queue *Queue) DeleteMessage(value []byte)   {
-
-	return
-}
-
-
-func (job *Job) initQueueList() {
-	job.List = make([]*Queue, job.Num)
-	var i int64
-	for i = 0; i < job.Num; i++ {
-		job.List[i] = NewQueue(job.Name, i)
+func (queue *queue) addAck(message Message) (err error)  {
+	con := queue.redisConn
+	dataByte ,err := json.Marshal(message)
+	if err != nil {
+		return
 	}
-}
+	expireTime := strconv.FormatInt(time.Now().Unix() + 60, 10)
+	var ok bool
+	err = con.Do(radix.Cmd(&ok, "ZADD", queue.doingTable, expireTime, string(dataByte)))
 
-
-func (job *Job) getList() *Queue {
-	//随机分配到某个list
-	key := RangeRand(0, job.Num - 1)
-	return job.List[key]
-}
-
-func (job *Job) Push(data interface{}) (err error) {
-	queue := job.getList()
-	message := Message{
-		Id: bson.NewObjectId().Hex() ,
-		Data: data,
-	}
-	err = queue.push(message)
-	return
-}
-
-
-func (job *Job) Handle(handleFunc func(message Message) bool) (data []byte, err error) {
-	var i int64
-	for i = 0 ; i < job.Num; i++ {
-		index := i
-		go func() {
-			for {
-				message, err := job.List[index].receive()
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-				handleFunc(message)
-			}
-		}()
-
+	if !ok {
+		err = errors.New("add ack fail")
 	}
 	return
 }
 
-func RangeRand(min, max int64) int64 {
+
+func (queue *queue) deleteMessage(message Message) (err error)   {
+	con := queue.redisConn
+	dataByte ,err := json.Marshal(message)
+	if err != nil {
+		return
+	}
+	var ok bool
+	err = con.Do(radix.Cmd(&ok, "ZREM", queue.doingTable, string(dataByte)))
+
+	if !ok {
+		err = errors.New("delete message fail")
+	}
+	return
+}
+
+
+
+
+func rangeRand(min, max int64) int64 {
 	if min > max {
 		panic("the min is greater than max!")
 	}
@@ -132,4 +277,3 @@ func RangeRand(min, max int64) int64 {
 	result, _ := rand.Int(rand.Reader, big.NewInt(max-min+1))
 	return min + result.Int64()
 }
-
